b3caf682108003c5453b56d11986b70bad4ca0ff
Caspar Meijer <c.j.meijer-1@student.tudelft.nl>
2018-09-27 09:45:57 +0200

    UpdateQ

--- src/tudelft/rl/mysolution/MyQLearning.java
+++ src/tudelft/rl/mysolution/MyQLearning.java
@@ -10,8 +10,14 @@ public class MyQLearning extends QLearning {
 
 	@Override
 	public void updateQ(State s, Action a, double r, State s_next, ArrayList<Action> possibleActions, double alfa, double gamma) {
-		// TODO Auto-generated method stub
-		
+		double Qmax = Double.MIN_VALUE;
+		for(Action action : possibleActions) {
+			if(this.getQ(s_next, action) > Qmax)
+				Qmax = this.getQ(s_next, action);
+		}
+
+		double newQ = this.getQ(s, a) + alfa * (r + gamma*Qmax-this.getQ(s,a));
+		this.setQ(s, a, newQ);
 	}
 
 }
