b24ad75fa3905a00bfcb62c9d42ad83477e3ee1c
Sam van Berkel <S.A.C.vanBerkel@student.tudelft.nl>
2018-09-27 09:46:59 +0200

    - Added a get random action for the e greedy algorithm - Added a get best action for the e greedy algorithm - Added a get e greedy action for the e greedy algorithm

--- src/tudelft/rl/mysolution/MyEGreedy.java
+++ src/tudelft/rl/mysolution/MyEGreedy.java
@@ -6,24 +6,49 @@ import tudelft.rl.EGreedy;
 import tudelft.rl.Maze;
 import tudelft.rl.QLearning;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MyEGreedy extends EGreedy {
 
 	@Override
 	public Action getRandomAction(Agent r, Maze m) {
-		//TODO to select an action at random in State s
-		return null;
+		ArrayList<Action> availableActions = m.getValidActions(r);
+
+		Collections.shuffle(availableActions);
+
+		return availableActions.get(0);
 	}
 
 	@Override
 	public Action getBestAction(Agent r, Maze m, QLearning q) {
-		//TODO to select the best possible action currently known in State s.
-		return null;
+		ArrayList<Action> availableActions = m.getValidActions(r);
+
+		double[] actionValues = q.getActionValues(r.getState(m), availableActions);
+
+		double maximumValue = Double.MIN_VALUE;
+		int maximumIndex = 0;
+
+		for (int i = 0; i < actionValues.length; i++) {
+			if (actionValues[i] > maximumValue) {
+				maximumValue = actionValues[i];
+				maximumIndex = i;
+			}
+		}
+		return availableActions.get(maximumIndex);
 	}
 
 	@Override
 	public Action getEGreedyAction(Agent r, Maze m, QLearning q, double epsilon) {
-		//TODO to select between random or best action selection based on epsilon.
-		return null;
+		epsilon = epsilon / r.nrOfActionsSinceReset + 1;
+
+		double probability = (1 - epsilon);
+
+		if (Math.random() <= probability) {
+			return getBestAction(r, m, q);
+		} else {
+			return getRandomAction(r, m);
+		}
 	}
 
 }
